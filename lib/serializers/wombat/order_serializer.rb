778ccc4cf649d689cb7e2fac3ebfc66427ea3a55
Artur Cygan <artur.cygan@netguru.pl>
2014-09-23 13:49:55 +0200

    Dry up srializer

--- lib/serializers/wombat/order_serializer.rb
+++ lib/serializers/wombat/order_serializer.rb
@@ -4,18 +4,14 @@ module Serializers
       def self.serialize(wombat_order)
         output = {}
         wombat_order.attributes.each do |key, value|
-          if value.respond_to?(:to_hash)
-            output[key] = value.to_hash
+          output[key] = if value.respond_to?(:to_hash)
+            value.to_hash
           elsif value.kind_of?(Enumerable)
-            output[key] = value.map do |nested_element|
-              if nested_element.respond_to?(:to_hash)
-                nested_element.to_hash
-              else
-                nested_element
-              end
+            value.map do |element|
+              element.respond_to?(:to_hash) ? element.to_hash : element
             end
           else
-            output[key] = value
+            value
           end
         end
         output
