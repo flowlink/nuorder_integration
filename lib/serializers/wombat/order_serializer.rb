f41b2bb16d99d6839734662aadb8289f734d53f4
Artur Cygan <artur.cygan@netguru.pl>
2014-09-23 16:44:40 +0200

    Remove nesting from wombat order serizalizer

--- lib/serializers/wombat/order_serializer.rb
+++ lib/serializers/wombat/order_serializer.rb
@@ -1,21 +1,19 @@
-module Serializers
-  module Wombat
-    class OrderSerializer
-      def self.serialize(wombat_order)
-        output = {}
-        wombat_order.attributes.each do |key, value|
-          output[key] = if value.respond_to?(:to_hash)
-            value.to_hash
-          elsif value.kind_of?(Enumerable)
-            value.map do |element|
-              element.respond_to?(:to_hash) ? element.to_hash : element
-            end
-          else
-            value
+module Wombat
+  class OrderSerializer
+    def self.serialize(wombat_order)
+      output = {}
+      wombat_order.attributes.each do |key, value|
+        output[key] = if value.respond_to?(:to_hash)
+          value.to_hash
+        elsif value.kind_of?(Enumerable)
+          value.map do |element|
+            element.respond_to?(:to_hash) ? element.to_hash : element
           end
+        else
+          value
         end
-        output
       end
+      output
     end
   end
 end
