8b2070e59adc142a8b5a11ccdc608d89c5d59f79
Artur Cygan <artur.cygan@netguru.pl>
2014-09-23 19:30:11 +0200

    Add cancel_order path

--- nuorder_endpoint.rb
+++ nuorder_endpoint.rb
@@ -12,4 +12,16 @@ class NuorderEndpoint < EndpointBase::Sinatra::Base
   get '/get_orders' do
     'NotImplemented'
   end
+
+  post '/cancel_order' do
+    begin
+      NuOrderServices::Order.new(@config).cancel!(@payload[:nuorder_id])
+      set_summary "Order has been cancelled in NuOrder"
+      result 200
+    rescue Exception => e
+      log_exception(e)
+      result 500, e.message
+    end
+  end
 end
+
